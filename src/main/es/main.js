e6e0b1c2ddf0882d6a46211b6bebe01dff7de9ba
JNourry-shodo <81433989+JNourry-shodo@users.noreply.github.com>
2021-03-30 14:42:16 +0200

    One to three translator

--- src/main/es/main.js
+++ src/main/es/main.js
@@ -1,9 +1,15 @@
-export const romanConverter = {
-    toRoman(arabic) {
-        return /*your code here*/ undefined;
-    },
-    fromRoman(roman) {
-        return /*your code here*/ undefined;
-    }
+const oneTranslator = (arabic) => {
+    let result = "";
+    if (arabic < 4 && arabic > 0)
+      for (; arabic !== 0; arabic = arabic - 1) result = result + "I";
+    return result;
 };
 
+export const romanConverter = {
+  toRoman(arabic) {
+    return oneTranslator(arabic);
+  },
+  fromRoman(roman) {
+    return /*your code here*/ undefined;
+  }
+};
